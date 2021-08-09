05f4beac63bf5860b4f67b4a31b6e13042086204
Joe Kramer <joe@kramergatos.com>
2021-08-08 23:08:03 -0500

    new header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,8 @@
-import Headline from './Headline'
 import styles from '../../styles/Header.module.css'
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>Kramer Gatos</div>
-      <Headline />
+      <img className={styles.header_background} src="/image/kramergatos-header.jpg" />
     </header>
   )
 }
