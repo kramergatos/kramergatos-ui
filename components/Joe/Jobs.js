7d950aa7a18554595bf95f88439ca825423d66c6
Joe Kramer <joe@kramergatos.com>
2021-08-20 15:24:03 -0500

    cover letter

--- components/Joe/Jobs.js
+++ components/Joe/Jobs.js
@@ -9,6 +9,10 @@ export default function Jobs(props) {
           <span className="fa fa-file-pdf"></span>
           <span className="button_label">Resume</span>
         </a>
+        <a className="button" href="/download/JoeKramerCoverLetter.pdf" target="_blank">
+          <span className="fa fa-file-pdf"></span>
+          <span className="button_label">Cover Letter</span>
+        </a>
         <div className={styles.jobs}>
           {props.data.map(item => (
             <Job data={item} key={item.id} />
