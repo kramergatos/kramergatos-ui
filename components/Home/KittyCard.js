0d0cf451a7531abaf697fa75730e2804b2c1b5d3
Joe Kramer <joe@kramergatos.com>
2021-08-20 22:15:02 -0500

    debug kitty sit card

--- components/Home/KittyCard.js
+++ components/Home/KittyCard.js
@@ -1,14 +1,14 @@
 import styles from '../../styles/Card.module.css'
 export default function KittyCard(props) {
   const pets = [
-    '/image/kitty/pet-sitting1.jpg',
-    '/image/kitty/pet-sitting2.jpg',
-    '/image/kitty/pet-sitting3.jpg',
-    '/image/kitty/pet-sitting4.jpg',
-    '/image/kitty/pet-sitting5.jpg',
-    '/image/kitty/pet-sitting6.jpg',
-    '/image/kitty/pet-sitting7.jpg',
-    '/image/kitty/pet-sitting9.jpg'
+    {id: "1", url: "/image/kitty/pet-sitting1.jpg"},
+    {id: "2", url: "/image/kitty/pet-sitting2.jpg"},
+    {id: "3", url: "/image/kitty/pet-sitting3.jpg"},
+    {id: "4", url: "/image/kitty/pet-sitting4.jpg"},
+    {id: "5", url: "/image/kitty/pet-sitting5.jpg"},
+    {id: "6", url: "/image/kitty/pet-sitting6.jpg"},
+    {id: "7", url: "/image/kitty/pet-sitting7.JPG"},
+    {id: "8", url: "/image/kitty/pet-sitting9.jpg"}
   ]
   return (
     <section>
@@ -17,7 +17,7 @@ export default function KittyCard(props) {
         <div className={styles.card_kitty}>
           <div>
             {pets.map(item => (
-              <img alt="cats" className={styles.card_kitty_image} src={item} />
+              <img alt="cats" className={styles.card_kitty_image} key={item.id} src={item.url} />
             ))}
             <span className={styles.card_title}>
               Professional International Kitty Sitters
