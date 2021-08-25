500f05de46b43ba32006591624506bee6124973a
Joe Kramer <joe@kramergatos.com>
2021-08-25 11:40:58 -0500

    gear splash img

--- pages/gear.js
+++ pages/gear.js
@@ -1,8 +1,10 @@
 import Layout from '../components/Global/Layout'
 import GearContent from '../components/Gear/Gear'
+import Splash from '../components/Gear/Splash'
 function Gear() {
   return (
     <Layout pageTitle="Gear - KramerGatos">
+      <Splash />
       <div className="section_gray">
         <GearContent />
       </div>
