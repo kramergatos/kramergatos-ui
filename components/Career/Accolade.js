0c7bbbcdba0eac14e68567bea14cdbf65605047e
Joe Kramer <joe.kramer@i2chain.com>
2022-02-19 15:06:40 -0600

    redesign accolades

--- components/Career/Accolade.js
+++ components/Career/Accolade.js
@@ -3,16 +3,18 @@ import styles from '../../styles/Accolades.module.css'
 export default function Accolade(props) {
   return (
     <div className={styles.accolade}>
-      <div className={styles.quote_start}>"</div>
+      <img alt={'person ' + props.data.company} className={styles.image} src={'/image/joe/accolades/' + props.data.image} />
       <div className={styles.description}>
+        <div className={styles.quote_start}>
+          <i className="fa-solid fa-quote-left"></i>
+        </div>
         {props.data.description}
+        <div className={styles.quote_end}>
+          <i className="fa-solid fa-quote-right"></i>
+        </div>
       </div>
-      <div className={styles.quote_end}>"</div>
       <div className={styles.title}>
         {props.data.name}
-      </div>
-      <div className={styles.date}>
-        {props.data.date}
       </div>
     </div>
   )
