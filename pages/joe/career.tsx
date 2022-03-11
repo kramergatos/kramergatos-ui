adde01ef0ffeeb6a8caab0f8a2ec801f2aad5a7e
Joe Kramer <joe.kramer@i2chain.com>
2022-03-11 11:16:04 -0600

    update job descrips

--- pages/joe/career.tsx
+++ pages/joe/career.tsx
@@ -65,7 +65,7 @@ function Career() {
           remote: 1,
           description: "Leading Front End development with offshore team on an innovative next generation security solution for enterprises that encrypts, injects rights management, and traceability into documents. Successfully implemented workflow, processes, tools, and communication to get the stalled project organized and back on track to deliver new features and bug fixes. Working with clients on testing, implementation, and features.",
           logo: "i2chain.png",
-          tech: ["AWS"]
+          tech: ["AWS", "Figma"]
         },
         {
           id: "501",
@@ -79,7 +79,7 @@ function Career() {
           remote: 1,
           description: "Developed next gen video player agnostic AdTech to display a variety of ad formats on top of or around the video player on desktop and mobile devices using TypeScript. Successfully completed the project months ahead of schedule and rolled out to clients using 20 different video providers proving the system to be player agnostic, adaptable, and configurable.",
           logo: "catapultx.jpeg",
-          tech: ["NodeJS", "ExpressJS", "TypeScript", "HTML5 / CSS3", "JWPlayer", "Akamai Player", "Avantis Player", "BridTV", "Fluid Player", "Rumble Player", "Video Intelligence Player", "YouTube", "AMP Player", "Google AMP", "AirTV Player", "Connatix Player"]
+          tech: ["NodeJS", "ExpressJS", "JavaScript", "TypeScript", "Vanilla JS", "HTML5 / CSS3", "JWPlayer", "Akamai Player", "Avantis Player", "BridTV", "Fluid Player", "Rumble Player", "Video Intelligence Player", "YouTube", "AMP Player", "Google AMP", "AirTV Player", "Connatix Player"]
         },
         {
           id: "502",
@@ -107,7 +107,7 @@ function Career() {
           remote: 1,
           description: "Strategy, architecture, and documentation of complete product overhaul of infrastructure, apps, and UI. Recruited, built and managed internal engineering team of 6. Collaborate with Sales, Account Management, and Customer Service to understand customer pain points with current product and desired features in the next version. Started rebuilding the product from scratch in NodeJS, NextJS, ReactJS, AWS.",
           logo: "wambi.png",
-          tech: ["AWS", "NodeJS", "NextJS", "ReactJS", "HTML5 / CSS3"]
+          tech: ["AWS", "JavaScript", "NodeJS", "NextJS", "ReactJS", "HTML5 / CSS3", "Adobe XD"]
         },
         {
           id: "504",
@@ -121,7 +121,7 @@ function Career() {
           remote: 2,
           description: "Worked with SRE Team to develop enterprise level internal tools. Built D3JS graphing & filtering dashboard UI for system performance monitoring of the entire Dell ecosystem from infrastructure to apps in a new mobile C#.NET Xamarin app. Built additional features into a Chrome Browser Extension, and the UI of C#.NET app used for product research.",
           logo: "dell.jpeg",
-          tech: ["HTML5 / CSS3", "C#.NET", "Xamarin", "D3JS"]
+          tech: ["HTML5 / CSS3", "C#.NET", "Xamarin", "D3JS", "Vanilla JS", "JavaScript"]
         },
         {
           id: "505",
@@ -135,7 +135,7 @@ function Career() {
           remote: 1,
           description: "Built API for app health monitoring and notifications with NodeJS, AWS Lambda. Built PHP scripts to utilize Brightcove API to retrieve, search, edit, & delete content in media library. Built API with NodeJS, AWS Lambda, DynamoDB to manage promo codes and winners and sent emails & push notifications to users with Leanplum.",
           logo: "nbc.png",
-          tech: ["AWS", "AWS Lambda", "AWS DynamoDB", "PHP"]
+          tech: ["AWS", "AWS Lambda", "AWS DynamoDB", "PHP", "NodeJS", "JavaScript"]
         },
         {
           id: "506",
@@ -146,9 +146,9 @@ function Career() {
           dateEnd: "08/2018",
           status: "Employee",
           remote: 1,
-          description:"Designed and built NodeJS, NextJS, ReactJS apps deployed on AWS for a variety of clients. Recruited, hired, trained, managed a cross functional team of 12. Sales, fund raising, and partnering with a gov contractor for RFPs.",
+          description: "Designed and built NodeJS, NextJS, ReactJS apps deployed on AWS for a variety of clients. Recruited, hired, trained, managed a cross functional team of 12. Sales, fund raising, and partnering with a gov contractor for RFPs.",
           logo: "nikola.png",
-          tech: ["AWS", "AWS CloudFront", "AWS S3", "AWS Lambda", "AWS DynamoDB", "NodeJS", "NextJS", "ReactJS", "HTML5 / CSS3"]
+          tech: ["AWS", "AWS Route53", "AWS CloudFront", "AWS S3", "AWS Lambda", "AWS DynamoDB", "JavaScript", "NodeJS", "NextJS", "ReactJS", "HTML5 / CSS3", "Adobe Photoshop"]
         },
         {
           id: "507",
@@ -160,9 +160,9 @@ function Career() {
           dateEnd: "01/2018",
           status: "Employee",
           remote: 1,
-          description:"Designed and built award winning, next gen, video player agnostic, interactive AdTech from scratch with NodeJS, ExpressJS, VAST, VPAID, AWS. Built a custom platform for content and campaign configuration. Recruited, hired, trained, and managed engineering team of 9. Sales Engineer for client demos and integrations as well as company M&A events including due diligence.",
+          description: "Designed and built award winning, next gen, video player agnostic, interactive AdTech from scratch with NodeJS, ExpressJS, VAST, VPAID, AWS. Built a custom platform for content and campaign configuration. Recruited, hired, trained, and managed engineering team of 9. Sales Engineer for client demos and integrations as well as company M&A events including due diligence.",
           logo: "kerv.png",
-          tech: ["AWS", "AWS CloudFront", "AWS S3", "AWS Lambda", "AWS DynamoDB", "MongoDB", "AWS Aurora DB", "AWS RedShift", "NodeJS", "ExpressJS", "Vanilla JS", "jQuery", "HTML5 / CSS3", "Brightcove", "The Platform (Comcast)", "JWPlayer", "Virtual Box"]
+          tech: ["AWS", "AWS Route53", "AWS CloudFront", "AWS S3", "AWS Lambda", "AWS DynamoDB", "AWS Aurora DB", "AWS RedShift", "MongoDB", "NodeJS", "ExpressJS", "JavaScript", "Vanilla JS", "jQuery", "HTML5 / CSS3", "Brightcove", "The Platform (Comcast)", "JWPlayer", "Virtual Box", "Adobe Photoshop"]
         },
         {
           id: "508",
@@ -173,7 +173,7 @@ function Career() {
           dateEnd: "09/2014",
           status: "Employee",
           remote: 0,
-          description:'Built a custom CMS platform with PHP, Symfony2. Built UI of video heavy web apps.',
+          description: "Built a custom CMS platform with PHP and Symfony2 for managing users, profiles, messages, and content. Built UI of video heavy web apps.",
           logo: "default.png",
           tech: ["HTML5 / CSS3", "PHP 5.4", "Symfony2", "Twig", "Doctrine", "Twitter Bootstrap", "MongoDB", "MySQL", "AJAX", "JavaScript", "jQuery", "XML", "YAML", "JSON", "Ubuntu 14.x", "CentOS6", "PHPStorm", "Virtual Box", "MySQL Workbench", "Apache", "NGINX"]
         },
@@ -187,7 +187,7 @@ function Career() {
           dateEnd: "06/2013",
           status: "Contract",
           remote: 0,
-          description:'Front End Development debugging and testing code for online educational courses as well as reverse engineering Flash based components into HTML5 and JavaScript.',
+          description:"Front End Development debugging and testing code for online educational courses as well as reverse engineering Flash based components into HTML5 and JavaScript.",
           logo: "flvs.png",
           tech: ["HTML5 / CSS3", "JSON", "XML", "JavaScript", "jQuery", "AJAX"]
         },
@@ -201,9 +201,9 @@ function Career() {
           dateEnd: "04/2013",
           status: "Contract",
           remote: 2,
-          description:'Full Stack Development designing and building a custom web application from scratch in C#.NET that has a public corporate home site as well as an authorized area for customers and employees to manage data and accounts. Business Analysis, application architecture, database design, web and graphic design. Initiated getting Git / GitHub implemented for our source control.',
+          description: "Full Stack Development designing and building a custom web application from scratch in C#.NET that has a public corporate home site as well as an authorized area for customers and employees to manage data and accounts. Business Analysis, application architecture, database design, web and graphic design. Initiated getting Git / GitHub implemented for our source control.",
           logo: "cc.jpg",
-          tech: ["C#.NET 4.5", "MVC 4", "SQL", "HTML5 / CSS3", "JavaScript", "jQuery", "AJAX", "XML", "Windows Server 2008", "SQL Server 2008/2012"]
+          tech: ["C#.NET 4.5", "MVC 4", "SQL", "HTML5 / CSS3", "JavaScript", "jQuery", "AJAX", "XML", "Windows Server 2008", "SQL Server 2008/2012", "Adobe Photoshop"]
         },
         {
           id: "511",
@@ -217,7 +217,7 @@ function Career() {
           remote: 2,
           description: "Full Stack Development of a new web app in PHP, JS, HTML, CSS that was a community site for the physical golf radar product that showed your performance super imposed on the golf course map, as well as leaderboard competition.",
           logo: "flightscope.png",
-          tech: ["PHP", "MySQL", "HTML5 / CSS3", "HTML5 Canvas", "JavaScript", "jQuery", "AJAX", "WordPress"]
+          tech: ["PHP", "MySQL", "HTML5 / CSS3", "HTML5 Canvas", "JavaScript", "jQuery", "AJAX", "WordPress", "Adobe Photoshop"]
         },
         {
           id: "512",
@@ -231,7 +231,7 @@ function Career() {
           remote: 0,
           description: "Worked closely with the design team for building emails and over 200 websites using JavaScript, jQuery, HTML, CSS for marketing landing pages of multimillion dollar campaigns. Debugging and researching issues in the AdTech platform company was developing for managing marketing campaigns.",
           logo: "integraclick.jpg",
-          tech: ["PHP 5.3", "Zend PHP", "HTML5 / CSS3", "JavaScript", "jQuery", "AJAX", "JSON", "XML", "Sencha ExtJS4", "WordPress"]
+          tech: ["PHP 5.3", "Zend PHP", "HTML5 / CSS3", "JavaScript", "jQuery", "AJAX", "JSON", "XML", "Sencha ExtJS4", "WordPress", "Adobe Photoshop"]
         },
         {
           id: "513",
@@ -243,45 +243,45 @@ function Career() {
           dateEnd: "12/2010",
           status: "Contract",
           remote: 2,
-          description:'Full stack development building out three websites and migrated content into a new CMS system. Served as liaison between Metrohm USA and the development team of the parent company Metrohm AG in Switzerland. Also built a simple Windows Console Application to track company computers. Content design and management, contact management, data migration into a CMS system, web services, SQL server, web design, graphic design, analytics, social media, marketing campaigns, communications.',
+          description: "Full stack development building out three websites and migrated content into a new CMS system. Served as liaison between Metrohm USA and the development team of the parent company Metrohm AG in Switzerland. Also built a simple Windows Console Application to track company computers. Content design and management, contact management, data migration into a CMS system, web services, SQL server, web design, graphic design, analytics, social media, marketing campaigns, communications.",
           logo: "musa.png",
           tech: ["Classic ASP", "C#.NET", "SQL", "HTML / CSS"]
         },
         {
           id: "514",
-          location:"Tampa, FL, USA",
-          title:"Web Developer",
-          company:"ReliaQuest",
+          location: "Tampa, FL, USA",
+          title: "Web Developer",
+          company: "ReliaQuest",
           url: "https://www.reliaquest.com/",
           dateStart: "07/2009",
           dateEnd: "02/2010",
           status: "Contract",
           remote: 2,
-          description:'Back End Development of websites and apps for a talent recruitment firm. Worked on three websites, including building the C# ASP.NET backends from scratch for two of them. Taught myself MS .NET Framework and related technologies. I also designed and managed databases, content design and management, graphic design.',
+          description: "Back End Development of websites and apps for a talent recruitment firm. Worked on three websites, including building the C# ASP.NET backends from scratch for two of them. Taught myself MS .NET Framework and related technologies. I also designed and managed databases, content design and management, graphic design.",
           logo: "reliaquest.png",
           tech: ["C#.NET", "SQL", "HTML / CSS", "AJAX"]
         },
         {
           id: "515",
-          location:"Tampa, FL, USA",
-          title:"Student",
-          company:"University of South Florida",
-          dateStart:"08/2007",
-          dateEnd:"12/2009",
-          status:"Student",
+          location: "Tampa, FL, USA",
+          title: "Student",
+          company: "University of South Florida",
+          dateStart: "08/2007",
+          dateEnd: "12/2009",
+          status: "Student",
           remote:0,
-          description:'Studied Management Information Systems, Engineering, Architecture. Alpha Epsilon Pi fraternity Psi Phi chapter; Vice President (Fall 2008 - Spring 2009). I dropped out of college early when I realized I was already learning and using the latest cutting edge technologies and becoming proficient in my work as a software engineer for local companies, and how much time and money I would save by skipping a slow, outdated, and expensive curriculum.',
+          description: "Studied Management Information Systems, Engineering, Architecture. Alpha Epsilon Pi fraternity Psi Phi chapter; Vice President (Fall 2008 - Spring 2009). I dropped out of college early when I realized I was already learning and using the latest cutting edge technologies and becoming proficient in my work as a software engineer for local companies, and how much time and money I would save by skipping a slow, outdated, and expensive curriculum.",
           logo: "usf.jpeg",
           tech: ["HTML / CSS"]
         },
         {
           id: "516",
           location:"Laurel, MD, USA",
-          title:"Owner and Founder",
-          company:"JKtheory Design",
+          title: "Owner and Founder",
+          company: "JKtheory Design",
           dateStart: "02/2003",
           dateEnd: "07/2006",
-          status:"Freelance",
+          status: "Freelance",
           remote: 1,
           description: "Freelance design and development of websites using PHP, HTML, CSS and tshirt printing for small local businesses and organizations while attending high school.",
           logo: "jktheory.png",
